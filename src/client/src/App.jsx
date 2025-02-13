2a9b969c210c6a5a418a4f33924982fa81f1ac00
Justin <tranjt@mail.uc.edu>
2025-02-13 15:56:17 -0500

    ESLint and Frontend Changes

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -1,79 +1,94 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import RestoreIcon from "@mui/icons-material/Restore";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 import "./App.css";
-import { IconButton, Button } from "@mui/material";
-import { Add, Inventory2, AccountCircle } from "@mui/icons-material";
-import axios from 'axios';
-import React, { useMemo, useState, useLayoutEffect, debounce } from 'react';
 
+import axios from "axios";
 
-// useEffect(()=> {
-//   setData(get_data());
-// })
+function get_data() {
+  axios
+    .get("http://localhost:5000/get_data", "car")
+    .then((response) => console.log(response.data));
+}
 
-// const data = {
-//   "test": 1
-// };
+// Add or remove qty of item (positive or negative integers)
+function update_item(item, qty) {
+  axios
+    .post("http://localhost:5000/item", { item: item, qty: qty })
+    .then((response) => console.log(response.data));
+}
 
-// Test App
-const App = () => {
-  const [data, setData] = useState(()=> {empty: 0});
+function get_item(item) {
+  axios
+    .get("http://localhost:5000/item", { item: item })
+    .then((response) => console.log(response.data));
+}
 
-  useLayoutEffect(() => {
-    setData(get_data())
-  }, []);
-
-  // API functions (test)
-  function get_data() {
-    console.log("get_data called")
-    axios.get('http://localhost:5000/get_data')
-    .then(response => console.log(response.data))
-  }
-
-  // Add or remove qty of item (positive or negative integers)
-  function update_item(item, qty) {
-    axios.post('http://localhost:5000/item', {'item': item, 'qty': qty})
-    .then(response => console.log(response.data))
-    setData(get_data())
-  }
-
-  function get_item(item) {
-    axios.get('http://localhost:5000/item', {'item': item})
-    .then(response => console.log(response.data ))
-  }
+function App() {
+  // const [count, setCount] = useState(0)
+  //add_item({})
   //get_data()
-  update_item("rice", 100)
-  //setData({"test": 1})
+  //add_item("car", 2)
+  update_item("rice", 200);
 
-  //const dataTest = {test: 1}
+  //add_item("car", 200)
+  //update_item("car", -10)
 
-  console.log(data.keys())
+  var fruits = [
+    { id: 1, name: "apple" },
+    { id: 2, name: "banana" },
+    { id: 2, name: "orange" },
+    { id: 2, name: "peach" },
+    { id: 2, name: "passion" },
+  ];
+
+  var listItems = fruits.map((fruit) => (
+    <li id="inventoryItem" key={fruit.id}>
+      {fruit.name} &nbsp;
+    </li>
+  ));
 
   return (
-    <body style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
-    <div style={{display: 'flex',  flexDirection: 'column', justifyContent:'center', alignItems:'center', height: '100vh', width: '100vw'}}>
-    <h1>Items and Quantities</h1>
-      <ul>
-         {data != null && 
-         Object.entries(data).map((item, quantity) => (
-          <li key={item}>
-            {item}: {quantity}
-            <Button
-            title={"Add 1"}
-            onClick={() => update_item(item, 1)}
+    <>
+      <div className="top">
+        <Box className="topBox">
+          <AppBar position="sticky" className="header">
+            <Toolbar>
+              <div className="headerTitle">
+                <h3>Internal Home Directory</h3>
+              </div>
+              <Button>Login</Button>
+            </Toolbar>
+          </AppBar>
+
+          <div className="body">
+            <div className="bodyTitle">
+              <h2>Welcome, Justin!</h2>
+            </div>
+          </div>
+
+          <BottomNavigation className="footer">
+            <a
+              href="https://github.com/tranjtGCP/Internal-Home-Directory"
+              className="footer"
             >
-            ADD  
-            </Button>
-            <Button
-            title={"Remove 1"}
-            onClick={() => update_item(item, -1)}
-            >
-            REMOVE  
-            </Button>
-          </li>
-        ))}
-      </ul>
-    </div>
-    </body>
+              GitHub
+            </a>
+          </BottomNavigation>
+        </Box>
+      </div>
+    </>
   );
-};
+}
 
 export default App;
