b87c640ff258e973025a4c72dbe37e4d63ef2986
Justin <tranjt@mail.uc.edu>
2025-02-14 12:09:10 -0500

    Added React Routing, changed some styling, refactored code

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -4,6 +4,7 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import MenuIcon from "@mui/icons-material/Menu";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
@@ -13,10 +14,8 @@ import LocationOnIcon from "@mui/icons-material/LocationOn";
 import { Unstable_NumberInput as NumberInput } from '@mui/base/Unstable_NumberInput';
 import "./App.css";
 import { Add, Inventory, AccountCircle } from "@mui/icons-material";
-import axios from 'axios';
-import React, { useMemo, useEffect, useState, useLayoutEffect, debounce, TextInput, Text } from 'react';
-import { Input } from "@mui/material";
-
+import axios from "axios";
+import React, { useMemo, useState, useLayoutEffect, debounce } from "react";
 
 // useEffect(()=> {
 //   setData(get_data());
@@ -40,42 +39,43 @@ const App = () => {
   }
 
   useLayoutEffect(() => {
-    get_data()
+    get_data();
   }, []);
 
   // API functions (test)
   function get_data() {
-    console.log("get_data called")
-    axios.get('http://localhost:5000/get_data')
-      .then(response => {
-        console.log(response.data)
-        setData(response.data)
-        return response.data
-      })
+    console.log("get_data called");
+    axios.get("http://localhost:5000/get_data").then((response) => {
+      console.log(response.data);
+      setData(response.data);
+      return response.data;
+    });
   }
 
   // Add or remove qty of item (positive or negative integers)
   function update_item(item, qty) {
-    axios.post('http://localhost:5000/item', { 'item': item, 'qty': qty })
-      .then(response => console.log(response.data))
-    get_data()
+    axios
+      .post("http://localhost:5000/item", { item: item, qty: qty })
+      .then((response) => console.log(response.data));
+    get_data();
   }
 
   function get_item(item) {
-    axios.get('http://localhost:5000/item', { 'item': item })
-      .then(response => console.log(response.data))
+    axios
+      .get("http://localhost:5000/item", { item: item })
+      .then((response) => console.log(response.data));
   }
 
   function save_changes() {
-    axios.post('http://localhost:5000/save_changes')
-      .then(response => { console.log(response.data) })
+    axios.post("http://localhost:5000/save_changes").then((response) => {
+      console.log(response.data);
+    });
   }
 
   function clear_changes() {
-    axios.delete('http://localhost:5000/save_changes')
-      .then(response => {
-        console.log(response.data)
-      })
+    axios.delete("http://localhost:5000/save_changes").then((response) => {
+      console.log(response.data);
+    });
   }
 
   //update_item("rice", 10)
@@ -90,58 +90,6 @@ const App = () => {
 
   return (
     <>
-      <body style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh', width: '100vw' }}>
-          <h1>Items and Quantities</h1>
-          <ul>
-            <Button
-              onClick={() => save_changes()}
-            >
-              Push Changes to File
-            </Button>
-            <form>
-              <label>Enter Item:
-                <input
-                  type="text"
-                  onChange={(e) => setInputItem(e.target.value)}
-                />
-              </label>
-            </form>
-            <NumberInput
-              aria-label="Demo number input"
-              placeholder="Type a number…"
-              onChange={(event, val) => setInputQty(val)}
-            />
-            <Button
-              title={"Update"}
-              onClick={() => handleUpdate()}
-              >
-              ADD NEW ITEM
-            </Button>
-
-            {data != null &&
-              Object.entries(data).map((item, quantity) => (
-                <li key={item}>
-                  {item[0]}: {item[1]}
-                  <Button
-                    title={"Add 1"}
-                    onClick={() => update_item(item[0], 1)}
-                  >
-                    ADD
-                  </Button>
-                  <Button
-                    title={"Remove 1"}
-                    onClick={() => update_item(item[0], -1)}
-                  >
-                    REMOVE
-                  </Button>
-                </li>
-              ))}
-
-          </ul>
-        </div>
-      </body>
-
       <div className="top">
         <Box className="topBox">
           <AppBar position="sticky" className="header">
@@ -149,13 +97,41 @@ const App = () => {
               <div className="headerTitle">
                 <h3>Internal Home Directory</h3>
               </div>
-              <Button>Login</Button>
+              <IconButton color="#426B1F" aria-label="add an alarm">
+                <AccountCircleIcon />
+              </IconButton>
             </Toolbar>
           </AppBar>
 
           <div className="body">
             <div className="bodyTitle">
               <h2>Welcome, Justin!</h2>
+            </div>
+            <div className="itemList">
+              <h2>Items and Quantities</h2>
+              <ul>
+                <Button onClick={() => save_changes()}>
+                  Push Changes to File
+                </Button>
+                {data != null &&
+                  Object.entries(data).map((item, quantity) => (
+                    <li key={item}>
+                      {item[0]}: {item[1]}
+                      <Button
+                        title={"Add 1"}
+                        onClick={() => update_item(item[0], 1)}
+                      >
+                        ADD
+                      </Button>
+                      <Button
+                        title={"Remove 1"}
+                        onClick={() => update_item(item[0], -1)}
+                      >
+                        REMOVE
+                      </Button>
+                    </li>
+                  ))}
+              </ul>
             </div>
           </div>
 
@@ -171,6 +147,6 @@ const App = () => {
       </div>
     </>
   );
-}
+};
 
 export default App;
