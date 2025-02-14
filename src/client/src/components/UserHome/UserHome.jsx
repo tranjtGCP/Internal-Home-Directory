6f90c22b113c241777767fd86c165dcf075e2f6b
Justin <tranjt@mail.uc.edu>
2025-02-14 13:14:50 -0500

    Stylized item list

--- src/client/src/components/UserHome/UserHome.jsx
+++ src/client/src/components/UserHome/UserHome.jsx
@@ -17,82 +17,99 @@ import axios from "axios";
 import React, { useMemo, useState, useLayoutEffect, debounce } from "react";
 import { BrowserRouter } from "react-router-dom";
 import "./UserHome.css";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import TextField from "@mui/material/TextField";
 
 const UserHome = () => {
+  // API functions (test)
+  const get_data = async () => {
+    console.log("get_data called");
+    await axios.get("http://localhost:5000/get_data").then((response) => {
+      console.log(response.data);
+      setData(response.data);
+      return response.data;
+    });
+  };
 
-    // API functions (test)
-    const get_data = async () => {
-      console.log("get_data called")
-      await axios.get('http://localhost:5000/get_data')
-        .then(response => {
-          console.log(response.data)
-          setData(response.data)
-          return response.data
-        })
-    }
-  
-    // Add or remove qty of item (positive or negative integers)
-    const update_item = async (item, qty) => {
-      await axios.post('http://localhost:5000/item', { 'item': item, 'qty': qty })
-        .then(response => console.log(response.data))
-      get_data()
-    }
-  
-    const get_item = async (item) => {
-      axios.get('http://localhost:5000/item', { 'item': item })
-        .then(response => console.log(response.data))
-    }
-  
-    const save_changes = async () => {
-      await axios.post('http://localhost:5000/save_changes')
-        .then(response => { console.log(response.data) })
-    }
-  
-    const clear_changes = async () => {
-      await axios.delete('http://localhost:5000/save_changes')
-        .then(response => {
-          console.log(response.data)
-        })
-    }
+  // Add or remove qty of item (positive or negative integers)
+  const update_item = async (item, qty) => {
+    await axios
+      .post("http://localhost:5000/item", { item: item, qty: qty })
+      .then((response) => console.log(response.data));
+    get_data();
+  };
 
-    const [data, setData] = useState(() => get_data());
-    const [inputItem, setInputItem] = useState("");
-    const [inputQty, setInputQty] = useState(0);
+  const get_item = async (item) => {
+    axios
+      .get("http://localhost:5000/item", { item: item })
+      .then((response) => console.log(response.data));
+  };
 
-    function handleUpdate() {
-      console.log(inputItem, inputQty)
-      update_item(inputItem, inputQty)
-    }
+  const save_changes = async () => {
+    await axios.post("http://localhost:5000/save_changes").then((response) => {
+      console.log(response.data);
+    });
+  };
 
-    useLayoutEffect(() => {
-      get_data()
-    }, []);
+  const clear_changes = async () => {
+    await axios
+      .delete("http://localhost:5000/save_changes")
+      .then((response) => {
+        console.log(response.data);
+      });
+  };
+
+  const [data, setData] = useState(() => get_data());
+  const [inputItem, setInputItem] = useState("");
+  const [inputQty, setInputQty] = useState(0);
+
+  function handleUpdate() {
+    console.log(inputItem, inputQty);
+    update_item(inputItem, inputQty);
+  }
+
+  useLayoutEffect(() => {
+    get_data();
+  }, []);
+
+  const testList = {
+    "Item 1": 1,
+    "Item 2": 2,
+    "Item 3": 10,
+    "Item 4": 100,
+  };
 
   return (
-    <div className="body">
+    <div className="userHomeTop">
       <div className="bodyTitle">
         <h2>Welcome, Justin!</h2>
       </div>
-      <div className="itemList">
+      <div className="body">
         <h2>Items and Quantities</h2>
-        <ul>
-          <Button onClick={() => save_changes()}>Push Changes to File</Button>
+        <Button onClick={() => save_changes()}>Push Changes to File</Button>
+        <div className="itemList">
           {data != null &&
-            Object.entries(data).map((item, quantity) => (
-              <li key={item}>
-                {item[0]}: {item[1]}
-                <Button title={"Add 1"} onClick={() => update_item(item[0], 1)}>
-                  ADD
-                </Button>
-                <Button
-                  title={"Remove 1"}
-                  onClick={() => update_item(item[0], -1)}
-                >
-                  REMOVE
-                </Button>
-              </li>
+            Object.entries(testList).map((item, quantity) => (
+              <div key={item} className="item">
+                <div className="itemTitle">
+                  <h3>{item[0]}</h3>
+                </div>
+                <div className="itemActions">
+                  <IconButton onClick={() => update_item(item[0], 1)}>
+                    <RemoveIcon></RemoveIcon>
+                  </IconButton>
+                  <TextField
+                    className="quantityInput"
+                    defaultValue={item[1]}
+                  ></TextField>
+                  <IconButton onClick={() => update_item(item[0], -1)}>
+                    <AddIcon></AddIcon>
+                  </IconButton>
+                </div>
+              </div>
             ))}
-        </ul>
+        </div>
       </div>
     </div>
   );
